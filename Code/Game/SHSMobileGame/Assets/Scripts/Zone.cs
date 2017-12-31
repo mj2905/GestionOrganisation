c1e3f6d7aa9b3e0893e8e068bc45ca90200f30ec
lkieliger <leandro.kieliger@epfl.ch>
2017-12-31 17:19:02 +0100

    Add Zone script to all zones

--- Code/Game/SHSMobileGame/Assets/Scripts/Zone.cs
+++ Code/Game/SHSMobileGame/Assets/Scripts/Zone.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 
-public class Zone
-{
+public class Zone : MonoBehaviour {
+
 	private int zoneId;
 	public int health;
 	public int team;
+	public Vector2 bottomLeftLatLon;
+	public Vector2 topRightLatLon;
+
+	void Start() {
+		
+	}
 
 	public Zone (int zoneId,int health, int team)
 	{
