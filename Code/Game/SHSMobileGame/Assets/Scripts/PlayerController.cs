2f7ef52a943e1101434f318adb4aec57d09c1bdf
mj2905 <marcandreolivier@gmail.com>
2018-02-28 13:48:11 +0100

    fixed geolocation

--- Code/Game/SHSMobileGame/Assets/Scripts/PlayerController.cs
+++ Code/Game/SHSMobileGame/Assets/Scripts/PlayerController.cs
@@ -7,19 +7,22 @@ public class PlayerController : MonoBehaviour
     public float speed;
 	public GameManager gameManager;
 	private GameObject currentZone; 
-	private static float defaultLongH = 6.56586f;//6.699792f;
-	private static float defaultLatV = 46.52018f;//46.55598f;
+	private static double defaultLongH = 6.56586;//6.699792f;
+	private static double defaultLatV = 46.52018;//46.55598f;
 
-	private static float topLeftLat = 46.52261f, topLeftLong = 6.56058f;
-	private static float topRightLat = 46.52261f, topRightLong = 6.5731f;
-	private static float botLeftLat = 46.51705f, botLeftLong = 6.56058f;
-	private static float botRightLat = 46.51705f, botRightLong = 6.5731f;
+	private static double topLeftLat = 46.52261, topLeftLong = 6.56058;
+	private static double topRightLat = 46.52261, topRightLong = 6.5731;
+	private static double botLeftLat = 46.51705, botLeftLong = 6.56058;
+	private static double botRightLat = 46.51705, botRightLong = 6.5731;
 
-	private static float epflCenterLat = 46.52018f, epflCenterLong = 6.56586f;
-	private static float epflCenterDifLat = epflCenterLat - defaultLatV, epflCenterDifLong = epflCenterLong - defaultLongH;
+	private static double horizontalDistance = topRightLong - topLeftLong;
+	private static double verticalDistance = topLeftLat - botLeftLat;
 
-	private static float lastPosLongH = epflCenterLong;
-	private static float lastPosLatV = epflCenterLat;
+	private static double epflCenterLat = 46.52018, epflCenterLong = 6.56586;
+	private static double epflCenterDifLat = epflCenterLat - defaultLatV, epflCenterDifLong = epflCenterLong - defaultLongH;
+
+	private static double lastPosLongH = epflCenterLong;
+	private static double lastPosLatV = epflCenterLat;
 
 	//Obtained by computing the unity size of the map
 	/*
@@ -33,9 +36,10 @@ public class PlayerController : MonoBehaviour
 	y = 77.15x/0.01252
 	 */
 
-	private const float hFactor = 77.15f;
-	private const float vFactor = 50.0f;
+	private const double hFactor = 2*77.15; //(1/l, 1/2h)/(l, h) = (1/2, 1/2). Need to multiply by 2 to get (1,1) and by the factors to reach the position in editor
+	private const double vFactor = 2*50.0;
 	bool locationWasStarted = false;
+	private Vector3 initialPosition;
 
 	public void BeginLocation() {
 		Input.location.Start (5.0f, 5.0f);
@@ -49,6 +53,7 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+		initialPosition = transform.position;
 		currentZone = null;
     }
 
@@ -69,21 +74,22 @@ public class PlayerController : MonoBehaviour
 		StopLocation ();
 	}
 
-	Vector3 GetMovement() {
+	Vector3 GetPosition() {
 		if (Input.location.isEnabledByUser && Input.location.status == LocationServiceStatus.Running) {
-			float posLongH = Input.location.lastData.longitude + epflCenterDifLong;
-			float posLatV = Input.location.lastData.latitude + epflCenterDifLat;
+			double posLongH = Input.location.lastData.longitude + epflCenterDifLong;
+			double posLatV = Input.location.lastData.latitude + epflCenterDifLat;
 
-			float moveHorizontal = (posLongH - lastPosLongH)*hFactor/(topRightLong - topLeftLong);
-			float moveVertical = (posLatV - lastPosLatV)*vFactor/(topLeftLat - botLeftLat);
+			double moveHorizontal = (posLongH - epflCenterLong)*hFactor/(topRightLong - topLeftLong);
+			double moveVertical = (posLatV - epflCenterLat)*vFactor/(topLeftLat - botLeftLat);
 
 			if (posLongH != lastPosLongH || posLatV != lastPosLatV) {
-				print ("location retrieved : lat:" + posLatV + " | long:" + posLongH + " | mh:" + moveHorizontal + "(~" + (957.9f * (posLongH - lastPosLongH) / (topRightLong - topLeftLong)) + "m)" + " | mv:" + moveVertical + "(~" + (618.2f * (posLatV - lastPosLatV)/(topLeftLat - botLeftLat)) + "m)");
+				print ("-----" + posLatV + " " + epflCenterLat + " " + topLeftLat + " " + botLeftLat);
+				print ("location retrieved : lat:" + posLatV + " | long:" + posLongH + " | mh:" + moveHorizontal + " | mv:" + moveVertical + "(~" + (957.9 * (posLongH - epflCenterLong) / (topRightLong - topLeftLong)) + "m)" + "(~" + (618.2 * (posLatV - epflCenterLat)/(topLeftLat - botLeftLat)) + "m)");
 			}
 
 			lastPosLatV = posLatV;
 			lastPosLongH = posLongH;
-			return new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			return new Vector3 ((float)moveHorizontal, 0.0f, (float)moveVertical);
 		} else {
 			return new Vector3 (0.0f, 0.0f, 0.0f);
 		}
@@ -91,8 +97,7 @@ public class PlayerController : MonoBehaviour
 
 	void FixedUpdate()
     {
-		Vector3 movement = GetMovement ();
-		gameObject.transform.position += movement;// * speed;
+		gameObject.transform.position = initialPosition + GetPosition();
 
 		if (currentZone != null && Input.GetKeyDown (KeyCode.T)) {
 			gameManager.AddTerminal(currentZone.name.Split('_')[0],gameObject.transform.localPosition.x,gameObject.transform.localPosition.z);
