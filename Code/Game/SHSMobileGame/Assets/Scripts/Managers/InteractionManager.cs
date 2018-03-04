83baa3bc62e5b1a865a8e530f8964a32065228a0
lkieliger <leandro.kieliger@epfl.ch>
2018-03-04 20:40:36 +0100

    Reorganize script files

--- Code/Game/SHSMobileGame/Assets/Scripts/Managers/InteractionManager.cs
+++ Code/Game/SHSMobileGame/Assets/Scripts/Managers/InteractionManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class InteractionManager : LocationListener {
 
 	public GameManager gameManager;
+	public FirebaseManager firebaseManager;
 
 	private Zone targetedZone;
 	private Terminal targetedTerminal;
