d0fe76953aa64a7735cf4b32f724971b6b8d6950
mj2905 <marcandreolivier@gmail.com>
2018-04-11 16:52:25 +0200

    text modified

--- Code/Game/SHSMobileGame/Assets/Scripts/Menu/LevelTabHandler.cs
+++ Code/Game/SHSMobileGame/Assets/Scripts/Menu/LevelTabHandler.cs
@@ -15,7 +15,7 @@ public class LevelTabHandler : MonoBehaviour {
 
 	private string[] texts = new string[]{
 		"Nothing special, no bonus for you.", "Your incredible mental capacities allow your terminals to pew pew more, and to better heal your zones (+5 damages for all terminals, +10 HP per zone heal).",
-		"You are the elite of the elite. You can repair terminals faster and your zones (+5 damages for all terminals, +5 HP per terminal heal, +20 HP per zone heal).",
+		"You are the elite of the elite. You can repair terminals and your zones faster (+5 damages for all terminals, +5 HP per terminal heal, +20 HP per zone heal).",
 		"Nobody understands what you are doing, great! Punch harder the zones with terminals, and better heal your zones (+10 damages for all terminals, +5 HP per terminal heal, +30 HP per zone heal).",
 		"Notice me, se-se-sempai. You can repair terminals faster and your zones (+10 damages for all terminals, +10 HP per terminal heal, +35 HP per zone heal)."
 	};
